8347a396aa379ec7555c75b45fd02ed9a00cba91
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-12 19:24:36 -0700

    Initial commit

---
+++ Script.js
@@ -0,0 +1,29 @@
+let word = "Hello";
+console.log(word);
+
+
+function myFunction() {
+    let number = 5;
+    let word = "hello";
+    console.log(number);
+    console.log(word);
+}
+myFunction();
+
+
+function myFunct() {
+    let num = 8;
+    if (true) {
+        console.log(num);
+    }
+}
+myFunct();
+
+
+function myFunc() {
+    if (true) {
+        var myBool = true;
+    }
+    console.log(myBool);
+}
+myFunc();
