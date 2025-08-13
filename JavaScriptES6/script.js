993f3117be40f385ebe84b1b88dba6463b150c71
Jack Sawyer <contact@jonathanfausset.com>
2025-08-13 11:21:58 -0500

    Updates and additions

---
+++ JavaScriptES6/script.js
@@ -0,0 +1,59 @@
+var next = ("====> NEXT <====\n");
+
+console.log('\n(1)\n');
+
+console.log('\n(2) ' + next);
+
+console.log('\n(3) ' + next);
+
+console.log('\n(4) ' + next);
+
+console.log('\n(5) ' + next);
+
+console.log('\n(6) ' + next);
+
+console.log('\n(7) ' + next);
+
+console.log('\n(8) ' + next);
+
+console.log('\n(9) ' + next);
+
+console.log('\n(10) ' + next);
+
+console.log('\n(11) ' + next);
+
+console.log('\n(12) ' + next);
+
+console.log('\n(13) ' + next);
+
+console.log('\n(14) ' + next);
+
+console.log('\n(15) ' + next);
+
+console.log('\n(16) ' + next);
+
+console.log('\n(17) ' + next);
+
+console.log('\n(18) ' + next);
+
+console.log('\n(19) ' + next);
+
+console.log('\n(20) ' + next);
+
+console.log('\n(21) ' + next);
+
+console.log('\n(22) ' + next);
+
+console.log('\n(23) ' + next);
+
+console.log('\n(24) ' + next);
+
+console.log('\n(25) ' + next);
+
+console.log('\n(26) ' + next);
+
+console.log('\n(27) ' + next);
+
+console.log('\n(28) ' + next);
+
+console.log('\n(29) ' + next);
